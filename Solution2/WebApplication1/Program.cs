1f9712b703e5ad947218428dfb16b30448372d5a
영진 최 <chyj0318@naver.com>
2019-01-16 14:17:35 +0900

    dddd

--- Solution2/WebApplication1/Program.cs
+++ Solution2/WebApplication1/Program.cs
@@ -19,6 +19,6 @@ namespace WebApplication1
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://*:80");
     }
 }
